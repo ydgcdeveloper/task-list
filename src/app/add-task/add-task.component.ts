81b286b5ab6a4d9dd06c5963b3cfb61089fcd857
Yan David <yan.gomez@qualud.es>
2023-02-22 18:48:27 -0500

    create add-task component

---
+++ src/app/add-task/add-task.component.ts
@@ -0,0 +1,43 @@
+import { HandleTaskService } from './../service/handle-task.service';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Task } from '../interface/task.interface';
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.css']
+})
+export class AddTaskComponent implements OnInit {
+  public taskForm: FormGroup;
+  @Output() taskEventShow = new EventEmitter<boolean>(false);
+  @Output() taskEventData = new EventEmitter<Task>();
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private taskService: HandleTaskService
+  ) {
+    this.taskForm = this.formBuilder.group({
+      text: ['', [Validators.required]],
+    })
+  }
+
+  get text() {
+    return this.taskForm.get('text');
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+
+  }
+
+  setAddNewTask() {
+    this.taskEventShow.emit(false);
+  }
+
+  addTask() {
+    this.taskEventData.emit({ text: this.text?.value })
+  }
+}
