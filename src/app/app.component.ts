eabddc0f422e1eb33e70ab005adada3ebbd23bf4
Yan David <yan.gomez@qualud.es>
2023-02-22 18:50:22 -0500

    set app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { HandleTaskService } from './service/handle-task.service';
+import { Component, OnInit } from '@angular/core';
+import { Task } from './interface/task.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'task-list';
+  public showAddTask: boolean = false;
+  public tasks: Task[] = [
+    { text: '#Important Write to email@gmail.com and tell @natasha about http://staging.alldone.app' }
+  ];
+
+  constructor(
+    private taskService: HandleTaskService
+  ) {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  presentAddTask() {
+    this.setShowAddTask(true);
+  }
+
+  setShowAddTask(value: boolean) {
+    this.showAddTask = value;
+  }
+
+  addNewTask(task: Task) {
+    console.log(task);
+    this.tasks.push(task)
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TaskComponent } from './task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTaskComponent,
+    TaskComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [AddTaskComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
